a0e78a7d244f17650b9c112a02f74f5c2cedb1c3
bondeje <jeffrey.bonde@gmail.com>
2024-10-11 14:25:08 -0700

    fixed linux for setnano.sh

--- setnano.sh
+++ setnano.sh
@@ -8,6 +8,9 @@ echo "" >> .nanorc
 if [ "NetBSD" = "$(uname -s)" ] ; then
 # NetBSD installs to /usr/pgk/share
 	cat /usr/pkg/share/nano/c.nanorc >> .nanorc ;
+elif [ "Linux" = "$(uname -s)" ] ; then
+# linux installs in /usr/share
+	cat /usr/share/nano/c.nanorc >> .nanorc ;
 else 
 # FreeBSD and OpenBSD install to /usr/local/share
 	cat /usr/local/share/nano/c.nanorc >> .nanorc ;
