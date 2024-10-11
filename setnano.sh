e8b63ec821191bd05d05a8f3301ef78759200ec1
bondeje <jeffrey.bonde@gmail.com>
2024-10-11 20:01:09 +0000

    fixed setnano.sh for NetBSD

--- setnano.sh
+++ setnano.sh
@@ -3,5 +3,12 @@ cd ~
 echo "set tabsize 4" > .nanorc
 echo "set linenumbers" >> .nanorc
 echo "set constantshow" >> .nanorc
+echo "" >> .nanorc
 
-cat /usr/local/share/nano/c.nanorc >> .nanorc
+if [ "NetBSD" = "$(uname -s)" ] ; then
+# NetBSD installs to /usr/pgk/share
+	cat /usr/pkg/share/nano/c.nanorc >> .nanorc ;
+else 
+# FreeBSD and OpenBSD install to /usr/local/share
+	cat /usr/local/share/nano/c.nanorc >> .nanorc ;
+fi
